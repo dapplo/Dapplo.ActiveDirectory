c39428ac7551d92ac56224030895bf24301ebd8b
Robin <robin@dapplo.net>
2016-01-08 13:54:40 +0100

    Renamed ActiveDirectory to ActiveDirectoryExtensions (using a class with the same name as a part of the namespace is problematic) and made the FindAll as an extension on Query. The properties which will be returned are not yet "optimized" and need to be specified, but at least they can be supplied as an array of enum values.

--- Dapplo.ActiveDirectory/ActiveDirectoryExtensions.cs
+++ Dapplo.ActiveDirectory/ActiveDirectoryExtensions.cs
@@ -25,27 +25,40 @@ using System;
 using System.Collections.Generic;
 using System.DirectoryServices;
 using System.Linq;
+using Dapplo.ActiveDirectory.Internal;
 
 namespace Dapplo.ActiveDirectory
 {
 	/// <summary>
 	/// A simple ActiveDirectory helper class
 	/// </summary>
-	public class ActiveDirectory
+	public static class ActiveDirectoryExtensions
 	{
-		public static readonly string[] UserProps = { "givenName", "sn", "department", "l", "telephoneNumber", "sAMAccountname", "mail" };
+		/// <summary>
+		/// Use the ActiveDirectory with the supplied domain to query
+		/// </summary>
+		/// <param name="query">Query</param>
+		/// <param name="domain">Domain for the LDAP server, if null the Environment.UserDomainName is used</param>
+		/// <param name="propertiesToLoad">An enumerable with the properties (as enum elements) to load</param>
+		/// <returns>IEnumerable with a dictionary for the properties</returns>
+		public static IEnumerable<IDictionary<string, object>> FindAll(this Query query, string domain = null, IEnumerable<Enum> propertiesToLoad = null)
+		{
+			return FindAll(query.Build(), domain, propertiesToLoad?.Select(x => x.EnumValueOf()));
+		}
 
 		/// <summary>
-		/// Query the LDAP server for the supplied domain
+		/// Use the ActiveDirectory with the supplied domain to query
 		/// </summary>
 		/// <param name="query">Query</param>
 		/// <param name="domain">Domain for the LDAP server, if null the Environment.UserDomainName is used</param>
 		/// <param name="propertiesToLoad">An enumerable with the properties to load, defaults include some user properties</param>
 		/// <returns>IEnumerable with a dictionary for the properties</returns>
-		public static IEnumerable<IDictionary<string, object>> FindAll(Query query, string domain = null, IEnumerable<string> propertiesToLoad = null)
+		public static IEnumerable<IDictionary<string, object>> FindAll(this Query query, string domain = null, IEnumerable<string> propertiesToLoad = null)
 		{
 			return FindAll(query.Build(), domain, propertiesToLoad);
 		}
+
+		public static readonly string[] UserProps = { "givenName", "sn", "department", "l", "telephoneNumber", "sAMAccountname", "mail" };
 
 		/// <summary>
 		/// Query the LDAP server for the supplied domain
